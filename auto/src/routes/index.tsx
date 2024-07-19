6bebcf1c4106b4b1a6bf2413288fa05f4326c7c6
samsonssali <ssali@jitpomi.com>
2024-07-18 21:10:24 -0600

    sidebar layout, video and pulse-green indicator

--- auto/src/routes/index.tsx
+++ auto/src/routes/index.tsx
@@ -1,18 +1,23 @@
-import {$, component$, useOnDocument, useOnWindow, useSignal, useTask$} from '@builder.io/qwik';
-import { DocumentHead, Link } from '@builder.io/qwik-city';
+import {$, component$, useOnDocument, useOnWindow, useSignal} from '@builder.io/qwik';
+import {DocumentHead, Link} from '@builder.io/qwik-city';
 import {
-  AntDesignAimOutlined, AntDesignGithubOutlined,
+  AntDesignGithubOutlined,
   Fa6SolidPlus,
   MaterialSymbolsDelete,
   MaterialSymbolsDesktopWindowsRounded,
   MaterialSymbolsFullscreen,
 } from '~/components/icons';
-import { largestRect } from 'rect-scaler';
+import {largestRect} from 'rect-scaler';
 
-const VIDEO_SRC =
-  'https://cdn.builder.io/o/assets%2F5b8073f890b043be81574f96cfd1250b%2F8b210c56974440649a0a78d4a3a0ddc5%2Fcompressed?apiKey=5b8073f890b043be81574f96cfd1250b&token=8b210c56974440649a0a78d4a3a0ddc5&alt=media&optimized=true';
+export interface AspectRatio {
+  ratio: number,
+  label: string,
+  ratioClass: string,
+}
 
-const aspectRatios = [
+const VIDEO_SRC = 'https://sg.fiverrcdn.com/packages_lp/cover_video.mp4';
+
+const aspectRatios: AspectRatio[] = [
   {
     ratio: 16 / 9,
     label: '16:9',
@@ -38,16 +43,18 @@ const aspectRatios = [
 const margin = 10;
 
 export default component$(() => {
+  const isVideo = useSignal<boolean>(true);
+  const sidebar = useSignal<boolean>(false);
   const container = useSignal<HTMLDivElement>();
   const tileContainer = useSignal<HTMLDivElement>();
-  const aspectRatio = useSignal(aspectRatios[0]);
+  const aspectRatio = useSignal<AspectRatio>(aspectRatios[0]);
 
   const width = useSignal(0);
   const height = useSignal(0);
 
   const getUsableDimensions = $(() => {
-    if (!container.value) return { usableWidth: 0, usableHeight: 0 };
-    const containerComputedStyle = getComputedStyle(container.value);
+    if (!tileContainer.value) return {usableWidth: 0, usableHeight: 0};
+    const containerComputedStyle = getComputedStyle(tileContainer.value);
     const containerPaddingX =
       parseInt(containerComputedStyle.paddingLeft.replace('px', ''), 10) +
       parseInt(containerComputedStyle.paddingRight.replace('px', ''), 10);
@@ -55,10 +62,10 @@ export default component$(() => {
       parseInt(containerComputedStyle.paddingTop.replace('px', ''), 10) +
       parseInt(containerComputedStyle.paddingBottom.replace('px', ''), 10);
 
-    const containerWidth = container.value.offsetWidth - containerPaddingX;
-    const containerHeight = container.value.offsetHeight - containerPaddingY;
+    const containerWidth = tileContainer.value.offsetWidth - containerPaddingX;
+    const containerHeight = tileContainer.value.offsetHeight - containerPaddingY;
 
-    return { containerWidth, containerHeight };
+    return {containerWidth, containerHeight};
   });
 
   const resizer = $((width: number, height: number, margin: number) => {
@@ -74,8 +81,10 @@ export default component$(() => {
 
   const resize = $(async () => {
     if (!tileContainer.value) return;
-    const containerDimensions = await getUsableDimensions();
     const tileCount = tileContainer.value.children.length;
+    if(!tileCount) return;
+    const containerDimensions = await getUsableDimensions();
+
     const aspectArray = aspectRatio.value.label.split(':');
     const aspectWidth = Number(aspectArray[0]);
     const aspectHeight = Number(aspectArray[1]);
@@ -92,14 +101,20 @@ export default component$(() => {
 
     width.value = dimensions.width - margin;
     height.value = dimensions.height - margin;
-   resizer(width.value, height.value, margin)
+    await resizer(width.value, height.value, margin);
   });
 
 
-
-  const addParticipantCamera = $(async() => {
+  const addParticipantCamera = $(async () => {
     if (!tileContainer.value) return;
     const card = document.createElement('div');
+    const video = document.createElement('video');
+    video.autoplay = true;
+    video.muted = true
+    video.className =  `video ${isVideo.value ? 'opacity-1': 'opacity-0'}`
+    video.src= VIDEO_SRC
+    video.loop = true
+    card.appendChild(video)
     tileContainer.value.appendChild(card);
     await resize();
   });
@@ -112,46 +127,101 @@ export default component$(() => {
     await resize();
   });
 
-  useTask$(async ({ track }) => {
-    track(() => aspectRatio.value);
-    await resize();
-  });
+  const showVideo = $(()=>{
+    if (!tileContainer.value) return;
+    const children = Array.from(tileContainer.value.children) as HTMLElement[];
+    isVideo.value = !isVideo.value
+    for (const child of children) {
+     const  videoElement = child.querySelector('video') as HTMLVideoElement;
+      if(isVideo.value) {
+        videoElement.classList.remove('opacity-0');
+        videoElement.classList.add('opacity-1');
+      } else {
+        videoElement.classList.remove('opacity-1');
+        videoElement.classList.add('opacity-0');
+      }
+    }
+  })
+
+  const showSideBar = $(async ()=>{
+    if (!tileContainer.value || !container.value) return;
+    const children = Array.from(tileContainer.value.children) as HTMLElement[];
+    sidebar.value = !sidebar.value;
+    const containerComputedStyle = getComputedStyle(tileContainer.value);
+    const containerPaddingY =
+      parseInt(containerComputedStyle.paddingTop.replace('px', ''), 10) +
+      parseInt(containerComputedStyle.paddingBottom.replace('px', ''), 10);
+    const screenHeight = tileContainer.value.offsetHeight -containerPaddingY;
+    if(sidebar.value && children.length > 1){
+      tileContainer.value.classList.add('bg-yellow-300')
+      const screen = children.pop() as HTMLElement;
+      screen.style.width = '60%'
+      screen.style.height = `${screenHeight}px`
+      screen.id = 'screen'
+      // const child = children.pop() as HTMLElement
+      // screen.appendChild(child)
+      container.value.innerHTML=''
+      tileContainer.value.innerHTML= '';
+      container.value?.appendChild(screen)
+      tileContainer.value.style.maxHeight = `${screenHeight}px`
+      container.value.appendChild(tileContainer.value)
+      for (const child of children) {
+        tileContainer.value.appendChild(child)
+      }
+      await resize()
+    } else {
+      const screen = document.getElementById('screen') as HTMLElement;
+      screen.id = '';
+      tileContainer.value.classList.remove('bg-yellow-300')
+      container.value.removeChild(screen);
+      tileContainer.value.appendChild(screen)
+      await resize()
+    }
+  })
+
+const changeAspectRatio = $(async (ratio: AspectRatio) => {
+  aspectRatio.value = ratio
+  await resize();
+})
+
+
+
 
   useOnDocument('DOMContentLoaded', addParticipantCamera);
   useOnWindow('resize', resize);
 
   return (
-    <div class={'relative bg-gray-50 h-full no-scrollbar'}>
-      <section class={'absolute inset-0 bg-gray-100 flex flex-col'}>
+    <div class={'relative bg-yellow-50 h-full no-scrollbar'}>
+      <section class={'absolute inset-0 bg-yellow-300 flex flex-col'}>
         <section class={'controls'}>
           <button class={'button bg-blue-500 text-white'} onClick$={addParticipantCamera}>
-            <Fa6SolidPlus />
+            <Fa6SolidPlus/>
           </button>
           <button class={'button bg-red-500 text-white'} onClick$={removeLastCamera}>
-            <MaterialSymbolsDelete />
+            <MaterialSymbolsDelete/>
           </button>
-          <button class={'button'}>
-            <MaterialSymbolsDesktopWindowsRounded />
+          <button class={`button ${isVideo.value ? 'is-active' : ''}`} onClick$={showVideo}>
+            <MaterialSymbolsDesktopWindowsRounded/>
           </button>
-          <button class={'button'}>
-            <MaterialSymbolsFullscreen />
+          <button class={`button  ${sidebar.value ? 'is-active' : ''}`} onClick$={showSideBar}>
+            <MaterialSymbolsFullscreen/>
           </button>
-          <button class={'button'}>
-            <AntDesignAimOutlined />
-          </button>
+
           {aspectRatios.map((ratio, index) => (
-            <button key={index} class={'button'} onClick$={() => aspectRatio.value = ratio}>
+            <button key={index}
+                    class={`button ${aspectRatio.value.ratioClass === ratio.ratioClass ? 'is-active' : ''}`}
+                    onClick$={()=>changeAspectRatio(ratio)}>
               {ratio.label}
             </button>
           ))}
           <Link href="https://github.com/jermsam/video-layouts" class="link">
             Github
-            <AntDesignGithubOutlined />
+            <AntDesignGithubOutlined/>
           </Link>
         </section>
         <section
           ref={container}
-          class={'flex flex-1 rounded-md bg-gray-50 max-h-full max-w-full p-5'}
+          class={'flex flex-1 justify-around rounded-md bg-yellow-400 max-h-full max-w-full p-5'}
         >
           <div
             class={'overflow-scroll justify-center flex flex-wrap content-center flex-1 rounded-md no-scrollbar'}
--- auto/tailwind.config.js
+++ auto/tailwind.config.js
@@ -7,17 +7,32 @@ export default {
         show: {
           '0%': {
             opacity: 0,
-            transform: 'scale(0.4) translateY(20px)'
+            transform: 'scale(0.4) translateY(20px)',
           },
           '100%': {
             opacity: 1,
-            transform: 'scale(1) translateY(0)'
-          }
-        }
+            transform: 'scale(1) translateY(0)',
+          },
+        },
+        'pulse-green': {
+          '0%': {
+            transform: 'scale(0.95)',
+            'box-shadow': '0 0 0 0 rgba(51, 217, 178, 1)',
+          },
+          '70%': {
+            transform: 'scale(1)',
+            'box-shadow': '0 0 0 20px rgba(51, 217, 178, 0)',
+          },
+          '100%': {
+            transform: 'scale(0.95)',
+            'box-shadow': '0 0 0 0 rgba(51, 217, 178, 0)',
+          },
+        },
       },
       animation: {
         show: 'show 0.4s ease',
-      }
+        'pulse-green': 'pulse-green 2s infinite'
+      },
     },
   },
   plugins: [],
